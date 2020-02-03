99c06e60731f7e2432f77fe2848f6528d764155f
Aslam Khan <aslam@visio-apps.com>
2020-02-03 17:08:36 +0530

    #Dependencies #Data Package Architecture

---
+++ app/src/main/java/com/visio/hdcnotes/data/local/db/AppDatabseHelper.java
@@ -0,0 +1,8 @@
+package com.visio.hdcnotes.data.local.db;
+//
+// Created by ASLAM on 03-02-2020.
+// Copyright (c) 2020 VisioApps Technology Pvt. Ltd. All rights reserved.
+//
+
+public class AppDatabseHelper {
+}
---
+++ app/src/main/java/com/visio/hdcnotes/data/local/pref/PrefHelper.java
@@ -0,0 +1,8 @@
+package com.visio.hdcnotes.data.local.pref;
+//
+// Created by ASLAM on 03-02-2020.
+// Copyright (c) 2020 VisioApps Technology Pvt. Ltd. All rights reserved.
+//
+
+public interface PrefHelper {
+}
