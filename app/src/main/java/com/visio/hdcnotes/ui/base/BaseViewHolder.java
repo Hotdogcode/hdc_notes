55ee5eb1553295ec29952bf435ccd055923394c6
Aslam Khan <aslam@visio-apps.com>
2020-02-07 10:10:40 +0530

    Added base class 1. BaseViewHolder

---
+++ app/src/main/java/com/visio/hdcnotes/ui/base/BaseViewHolder.java
@@ -0,0 +1,8 @@
+package com.visio.hdcnotes.ui.base;
+//
+// Created by ASLAM on 07-02-2020.
+// Copyright (c) 2020 VisioApps Technology Pvt. Ltd. All rights reserved.
+//
+
+public class BaseViewHolder {
+}
