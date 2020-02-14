c3a0d573f45fb00a0319bacdf929c195681334e2
Aslam Khan <aslam@visio-apps.com>
2020-02-14 18:13:06 +0530

    Add MainActvity Package 1. MainViewModel 2. MainNavigator 3. MainActivity 4. MainActivityModule 5. activity_main 6. ViewModelProviderFactory

---
+++ app/src/main/java/com/visio/hdcnotes/ui/main/MainViewModel.java
@@ -0,0 +1,8 @@
+package com.visio.hdcnotes.ui.main;
+//
+// Created by ASLAM on 14-02-2020.
+// Copyright (c) 2020 VisioApps Technology Pvt. Ltd. All rights reserved.
+//
+
+public class MainViewModel {
+}
